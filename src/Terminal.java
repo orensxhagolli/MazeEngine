1a4241a23e78f962e8180b79931af6c0479aec25
Orens Xhagolli <orens.xhagolli@hotmail.com>
2015-07-15 09:12:31 -0400

    Fixed!

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,22 +1,15 @@
 /**
- * Created by Orens on 7/14/2015.
+ * Created by Orens on 7/15/2015.
  */
 public class Terminal {
     public static void main(String[] args) {
-        Maze m = new Maze(6, 6);
-        String[][] brd = new String[6][6];
-        for (Coordinate coordinate: m.getBoard().keySet()){
-            if(m.getBoard().get(coordinate).isWall()){
-                brd[coordinate.getX()][coordinate.getY()] = "x";
-            }
-            else {
-                brd[coordinate.getX()][coordinate.getY()] = ".";
-            }
-            System.out.println(m.getBoard().get(coordinate).getNeighbors());
-        }
-        for (int i = 0; i < brd.length; i ++){
-            for (int j = 0; j < brd[i].length; j ++){
-                System.out.print(brd[i][j]);
+        //Use this as a text-based interface for the engine. Do not refer to this class in your implementation
+        System.out.println("Welcome to THE MAZE ENGINE");
+        System.out.println("Developed by Orens Xhagolli and Noah Rubin");
+        Maze maze = new Maze(10, 10);
+        for(int x = 0; x < 10; x++){
+            for(int y = 0; y < 10; y++){
+                System.out.print(maze.getBoard()[x][y].isWall() ? "x" : ".");
             }
             System.out.println();
         }
